77375965cb53542f85f7a9e9a3467dd1dc7bfce7
uk <o.office8945@gmail.com>
2025-04-08 10:20:59 +0900

    'jsxGrammar'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-function App() {
-  const [count, setCount] = useState(0);
+//import logo from "./vite.svg";
+import "./App.css"; //css파일 쓰려면 상단에서 import 'css파일 경로'
 
-  return <div></div>;
+function App() {
+  let post = "강남 우동 맛집"; //서버에서 가져온 데이터라고 가정
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4 style={{ color: "red", fontSize: "16px" }}>블로그</h4>
+      </div>
+      <h4>{post}</h4>
+    </div>
+  );
 }
 
 export default App;
